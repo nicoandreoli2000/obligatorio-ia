fce2601efea5c6739e049dd6a40817d99e9007b1
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2023-07-09 09:40:36 -0300

    feat: last test

--- ConnectFour/MinimaxAgent.py
+++ ConnectFour/MinimaxAgent.py
@@ -84,4 +84,3 @@ class MinimaxAgent(Agent):
             score += 10
 
         return score
-
--- ConnectFour/play.py
+++ ConnectFour/play.py
@@ -1,28 +1,28 @@
 from spaceGPT_agent_obf import SpaceGPTAgent
 
-def play_vs_other_agent(env, agent1, agent2):
+def play_vs_other_agent(env, agent1, agent2, render=True):
     done = False
     obs = env.reset()
     winner = 0
     while not done:
-        env.render()
+        if render: env.render()
         action = agent1.next_action(obs)
         obs, _, done, _ = env.step(action)
 
-        env.render()
+        if render: env.render()
         if not done:
             next_action = agent2.next_action(obs)
             _, _, done, _ = env.step(next_action)
 
-    env.render()
+    if render: env.render()
     winner = env._grid.winner
     final_msg = "Player " + str(winner) + " WON!!!" if winner != 0 else "It's a tie!" 
-    print(final_msg)
+    if render: print(final_msg)
     return final_msg
 
-def play_vs_loaded_agent(env, agent):
+def play_vs_loaded_agent(env, agent, render=True):
     enemy_agent = load_enemy_agent()
-    return play_vs_other_agent(env, agent, enemy_agent)
+    return play_vs_other_agent(env, agent, enemy_agent, render)
 
 def load_enemy_agent():
     return SpaceGPTAgent(2, 4)
